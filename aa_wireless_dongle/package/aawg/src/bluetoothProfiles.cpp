41a6e467b6e90fb8b07e0e50b79dc6a700c1398f
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2023-05-28 06:23:17 +0000

    Launch Wireless AA on phone via bluetooth

--- aa_wireless_dongle/package/aawg/src/bluetoothProfiles.cpp
+++ aa_wireless_dongle/package/aawg/src/bluetoothProfiles.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <thread>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
+#include "common.h"
 #include "bluetoothProfiles.h"
+
+#include <google/protobuf/message_lite.h>
+#include "proto/WifiStartRequest.pb.h"
+#include "proto/WifiInfoResponse.pb.h"
 
 static constexpr const char* INTERFACE_BLUEZ_PROFILE = "org.bluez.Profile1";
 
@@ -13,6 +22,139 @@ BluezProfile::BluezProfile(DBus::Path path): DBus::Object(path) {
 }
 #pragma endregion BluezProfile
 
+
+#pragma region AAWirelessLauncher
+class AAWirelessLauncher {
+public:
+    AAWirelessLauncher(int fd): m_fd(fd) {};
+
+    void launch() {
+        // Make fd blocking
+        int fd_flags = fcntl(m_fd, F_GETFL);
+        fcntl(m_fd, F_SETFL, fd_flags & ~O_NONBLOCK);
+
+        WifiInfo wifiInfo = Config::instance()->getWifiInfo();
+
+        printf("Sending WifiStartRequest\n");
+        WifiStartRequest wifiStartRequest;
+        wifiStartRequest.set_ip_address(wifiInfo.ipAddress);
+        wifiStartRequest.set_port(wifiInfo.port);
+
+        SendMessage(MessageId::WifiStartRequest, &wifiStartRequest);
+
+        MessageId messageId = ReadMessage();
+
+        if (messageId != MessageId::WifiInfoRequest) {
+            printf("Expected WifiInfoRequest, got %s (%d). Abort.\n", MessageName(messageId), messageId);
+            return;
+        }
+
+        printf("Sending WifiInfoResponse\n");
+        WifiInfoResponse wifiInfoResponse;
+        wifiInfoResponse.set_ssid(wifiInfo.ssid);
+        wifiInfoResponse.set_key(wifiInfo.key);
+        wifiInfoResponse.set_bssid(wifiInfo.bssid);
+        wifiInfoResponse.set_security_mode(wifiInfo.securityMode);
+        wifiInfoResponse.set_access_point_type(wifiInfo.accessPointType);
+
+        SendMessage(MessageId::WifiInfoResponse, &wifiInfoResponse);
+
+        ReadMessage();
+        ReadMessage();
+    }
+
+private:
+    enum class MessageId {
+        Invalid = -1,
+        WifiStartRequest = 1,
+        WifiInfoRequest = 2,
+        WifiInfoResponse = 3,
+        WifiVersionRequest = 4,
+        WifiVersionResponse = 5,
+        WifiConnectStatus = 6,
+        WifiStartResponse = 7,
+    };
+    std::string MessageName(MessageId messageId) {
+        switch (messageId) {
+            case MessageId::WifiStartRequest:
+                return "WifiStartRequest";
+            case MessageId::WifiInfoRequest:
+                return "WifiInfoRequest";
+            case MessageId::WifiInfoResponse:
+                return "WifiInfoResponse";
+            case MessageId::WifiVersionRequest:
+                return "WifiVersionRequest";
+            case MessageId::WifiVersionResponse:
+                return "WifiVersionResponse";
+            case MessageId::WifiConnectStatus:
+                return "WifiConnectStatus";
+            case MessageId::WifiStartResponse:
+                return "WifiStartResponse";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    void SendMessage(MessageId messageId, google::protobuf::MessageLite* message) {
+        uint16_t messageSize = (uint16_t)message->ByteSizeLong();
+        uint16_t length = messageSize + 4;
+
+        unsigned char* buffer = new unsigned char[length];
+
+        uint16_t networkShort = 0;
+        networkShort = htons(messageSize);
+        memcpy(buffer, &networkShort, sizeof(networkShort));
+
+        networkShort = htons(static_cast<uint16_t>(messageId));
+        memcpy(buffer + 2, &networkShort, sizeof(networkShort));
+
+        message->SerializeToArray(buffer + 4, messageSize);
+
+        ssize_t wrote = write(m_fd, buffer, length);
+        if (wrote < 0) {
+            printf("Error sending %s, messageId: %d\n", MessageName(messageId).c_str(), messageId);
+        }
+        else {
+            printf("Sent %s, messageId: %d, wrote %d bytes\n", MessageName(messageId).c_str(), messageId, wrote);
+        }
+
+        delete buffer;
+    }
+
+    MessageId ReadMessage() {
+        uint16_t networkShort = 0;
+        ssize_t readBytes;
+
+        readBytes = read(m_fd, &networkShort, 2);
+        if (readBytes != 2) {
+            // Could not read 2 bytes. Do something.
+            printf("Error reading length, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
+            return MessageId::Invalid;
+        }
+        uint16_t length = ntohs(networkShort);
+
+        readBytes = read(m_fd, &networkShort, 2);
+        if (readBytes != 2) {
+            // Could not read 2 bytes. Do something.
+            printf("Error reading message id, read bytes: %d, errno: %s\n", readBytes, strerror(errno));
+            return MessageId::Invalid;
+        }
+        MessageId messageId = static_cast<MessageId>(ntohs(networkShort));
+
+        printf("Read %s. length: %d, messageId: %d\n", MessageName(messageId).c_str(), length, messageId);
+        
+        unsigned char* buffer = new unsigned char[length];
+        readBytes = read(m_fd, buffer, length);
+
+        delete buffer;
+
+        return messageId;
+    }
+
+    int m_fd;
+};
+#pragma endregion AAWirelessLauncher
+
 #pragma region AAWirelessProfile
 void AAWirelessProfile::Release() {
     printf("AA Wireless Release\n");
@@ -21,6 +163,9 @@ void AAWirelessProfile::Release() {
 void AAWirelessProfile::NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) {
     printf("AA Wireless NewConnection\n");
     printf("Path: %s, fd: %d\n", path.c_str(), fd->descriptor());
+
+    AAWirelessLauncher(fd->descriptor()).launch();
+    printf("Bluetooth launch sequence completed\n");
 }
 
 void AAWirelessProfile::RequestDisconnection(DBus::Path path) {
--- aa_wireless_dongle/package/aawg/src/bluetoothProfiles.h
+++ aa_wireless_dongle/package/aawg/src/bluetoothProfiles.h
@@ -13,24 +13,26 @@ protected:
 
 
 class AAWirelessProfile: public BluezProfile {
+public:
+    static std::shared_ptr<AAWirelessProfile> create(DBus::Path path);
+
+private:
     void Release() override;
     void NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) override;
     void RequestDisconnection(DBus::Path path) override;
 
     AAWirelessProfile(DBus::Path path);
-
-public:
-    static std::shared_ptr<AAWirelessProfile> create(DBus::Path path);
 };
 
 
 class HSPHSProfile: public BluezProfile {
+public:
+    static std::shared_ptr<HSPHSProfile> create(DBus::Path path);
+
+private:
     void Release() override;
     void NewConnection(DBus::Path path, std::shared_ptr<DBus::FileDescriptor> fd, DBus::Properties fdProperties) override;
     void RequestDisconnection(DBus::Path path) override;
 
     HSPHSProfile(DBus::Path path);
-
-public:
-    static std::shared_ptr<HSPHSProfile> create(DBus::Path path);
 };
