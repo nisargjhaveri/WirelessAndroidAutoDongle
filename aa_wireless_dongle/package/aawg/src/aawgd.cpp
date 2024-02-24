7696a4023c60e990041d7a2e05e7aa42d1e64be9
Ioniq3 <156689157+Ioniq3@users.noreply.github.com>
2024-02-24 18:16:58 +0100

    request done

--- aa_wireless_dongle/package/aawg/src/aawgd.cpp
+++ aa_wireless_dongle/package/aawg/src/aawgd.cpp
@@ -18,8 +18,8 @@ int main(void) {
     while (true) {
         // Per connection setup and processing
         if (const char* env_p = std::getenv("AAWG_CONNECTION_WAIT_FOR_ACCESSORY")) {
-            UsbManager::instance().enableDefaultAndWaitForAccessroy();
             Logger::instance()->info("Waiting for the accessory to connect first\n");
+            UsbManager::instance().enableDefaultAndWaitForAccessroy();
         }
 
         AAWProxy proxy;
