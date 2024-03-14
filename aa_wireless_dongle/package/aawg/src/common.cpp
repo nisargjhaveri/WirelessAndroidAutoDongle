c1379f7bcb092c1d49415d187e7745d15a991b88
Miguel Fuertes <hkfuertes@gmail.com>
2024-03-14 20:00:07 +0100

    default wlans

--- aa_wireless_dongle/package/aawg/src/common.cpp
+++ aa_wireless_dongle/package/aawg/src/common.cpp
@@ -39,8 +39,8 @@ std::string Config::getMacAddress(std::string interface) {
 
 WifiInfo Config::getWifiInfo() {
     return {
-        getenv("AAWG_WIFI_SSID", "AndroidAuto"),
-        getenv("AAWG_WIFI_PASSWORD", "1234567890"),
+        getenv("AAWG_WIFI_SSID", "AAWirelessDongle"),
+        getenv("AAWG_WIFI_PASSWORD", "ConnectAAWirelessDongle"),
         getenv("AAWG_WIFI_BSSID", getMacAddress("wlan0")),
         SecurityMode::WPA2_PERSONAL,
         AccessPointType::DYNAMIC,
