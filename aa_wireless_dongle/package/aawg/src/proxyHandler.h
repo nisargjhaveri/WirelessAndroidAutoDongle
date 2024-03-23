4562801ea2da4db91ef431e6f7b78be1393dee4e
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2024-03-24 01:46:53 +0530

    Add TCP read timeout, more reliably stop forwarding by interrupting the threads
    - Add read timeout for the TCP connection to help detect wifi disconnection or other failures
    - When USB/TCP error is detected, interrupt the other thread to stop waiting on the read/write operation

--- aa_wireless_dongle/package/aawg/src/proxyHandler.h
+++ aa_wireless_dongle/package/aawg/src/proxyHandler.h
@@ -16,12 +16,16 @@ private:
 
     void handleClient(int server_fd);
     void forward(ProxyDirection direction, std::atomic<bool>& should_exit);
+    void stopForwarding(std::atomic<bool>& should_exit);
 
     ssize_t readFully(int fd, unsigned char *buf, size_t nbyte);
     ssize_t readMessage(int fd, unsigned char *buf, size_t nbyte);
 
     int m_usb_fd = -1;
     int m_tcp_fd = -1;
+
+    std::optional<std::thread> m_usb_tcp_thread = std::nullopt;
+    std::optional<std::thread> m_tcp_usb_thread = std::nullopt;
 
     std::atomic<bool> m_log_communication = false;
 };
